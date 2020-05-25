9dac3014ee60985319b9c842ae4c2285730c8603
Stas Karpov <karpovstanislav1@gmail.com>
2020-05-25 21:36:55 +0200

    Updates

---
+++ precache-manifest.516457992ba500bb63761ad4da22e6ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e39dbacc7ccd24725aef477897f80b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "510133601a21e125c518",
+    "url": "/static/js/2.3a912e00.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.3a912e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2cc9fe6a0f93bede3b",
+    "url": "/static/js/main.309122ba.chunk.js"
+  },
+  {
+    "revision": "a0e1ac441e3638d38370",
+    "url": "/static/js/runtime-main.a6034e4a.js"
+  }
+]);
